0b3c5907b2f9d15eacfd026210cdba1e12de496e
Jiaowenhui <523947655@qq.com>
2017-10-11 09:50:30 +0800

    切图

--- weixinsrc/src/components/login/index.js
+++ weixinsrc/src/components/login/index.js
@@ -3,13 +3,81 @@
  */
 import React from 'react';
 import { connect } from 'react-redux';
+import loginbg from "../../img/8.png";
+import { withRouter } from 'react-router-dom';
+import './style.css';
+
+
+export class PageForm extends Component {
+    render(){
+        const { handleSubmit,onClickLogin,pristine,submitting } = this.props;
+
+        return (
+            <Form
+                className="loginForm"
+                onSubmit={handleSubmit(onClickLogin)}
+                >
+                <div className="logo">
+                    <span className="logoimg"></span>
+                    <span className="logospan">LOGO</span>
+                </div>
+                <div className="li" >
+                    <span className="icon">
+                        <img src="newimg/25.png" alt='' />
+                    </span>
+                    <Field
+                        name="phonenumber"
+                        id="phonenumber"
+                        placeholder="User name"
+                        type="text"
+                        component={ InputValidation }
+                        validate={[ required, phone ]}
+                    />
+                </div>
+                <div className="li">
+                    <span className="icon">
+                        <img src="newimg/26.png" alt='' />
+                    </span>
+                    <Field
+                        name="password"
+                        id="password"
+                        placeholder="Password"
+                        type="password"
+                        component={ InputValidation }
+                        validate={[ required ]}
+                    />
+                </div>
+
+                <br/>
+                <br/>
+
+                <div className="submitBtn">
+                    <span
+                        className="btn Default"
+                        disabled={pristine || submitting}
+                        onClick={handleSubmit(onClickLogin)}
+                        >
+                        登录
+                    </span>
+                    
+
+                </div>
+            </Form>
+        )
+    }
+}
+
+PageForm = reduxForm({
+    form: 'LoginPageForm'
+})(PageForm);
 
 class Page extends React.Component {
     
     render() {
         return (
-            <div className="indexPage AppPage">
-                这里是首页
+            <div className="loginPage AppPage">
+                <img src={loginbg} className="bg" />
+                <div className="loginform"></div>
             </div>
         );
     }
--- weixinsrc/src/reducers/index.js
+++ weixinsrc/src/reducers/index.js
@@ -2,7 +2,8 @@ import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import { routerReducer } from 'react-router-redux';
 import { polyglotReducer } from 'redux-polyglot';
+import userlogin from './userlogin';
 
 export default combineReducers({
-
+	userlogin
 });
